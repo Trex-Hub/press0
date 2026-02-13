a4d832eef85830630a875379df2291caaa46d30b
Devendra Mishra <devendrapkmishra@gmail.com>
2026-02-13 17:16:20 +0530

    feat(next-config): add transpilePackages option for utility package

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   cacheComponents: true,
   reactCompiler: true,
+  transpilePackages: ['@repo/utils'],
 };
 
 export default nextConfig;
